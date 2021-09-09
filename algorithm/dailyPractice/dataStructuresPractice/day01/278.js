ae632c7f258298e4b08725bc7c91fbc4272906f4
wuyangfan <1102042793@qq.com>
2021-09-09 23:28:49 +0800

    数据结构练习day01代码提交

--- algorithm/dailyPractice/dataStructuresPractice/day01/278.js
+++ algorithm/dailyPractice/dataStructuresPractice/day01/278.js
@@ -1,1 +1,35 @@
 // 278. 第一个错误的版本
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let left = 1,
+            right = n,
+            mid;
+        while (left < right) {
+            mid = ((right - left) >> 1) + left;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    };
+};
