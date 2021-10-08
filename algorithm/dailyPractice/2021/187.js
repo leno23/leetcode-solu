8889b473a7eb49012adf9a771c8d99185e458794
wuyangfan <1102042793@qq.com>
2021-10-08 23:55:17 +0800

    每日一题题解

---
+++ algorithm/dailyPractice/2021/187.js
@@ -0,0 +1,26 @@
+/*
+187. 重复的DNA序列
+所有 DNA 都由一系列缩写为 'A'，'C'，'G' 和 'T' 的核苷酸组成，
+例如："ACGAATTCCG"。
+在研究 DNA 时，识别 DNA 中的重复序列有时会对研究非常有帮助。
+
+编写一个函数来找出所有目标子串，目标子串的长度为 10，且在 DNA 字符串
+ s 中出现次数超过一次。
+*/
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function (s) {
+    if (s.length < 10) return [];
+    let map = new Map(),
+        ret = new Set();
+    for (let i = 0; i < s.length; i++) {
+        if (!s[i + 9]) break;
+        let str = s.slice(i, i + 10);
+        map.set(str, (map.get(str) || 0) + 1);
+        // 去重
+        if (map.get(str) > 1) ret.add(str);
+    }
+    return [...ret];
+};
