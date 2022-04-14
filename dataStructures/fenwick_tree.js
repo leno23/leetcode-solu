1dd3da4f5db2f2380ad68390a58a8f56a0871bab
wuyangfan <1102042793@qq.com>
2022-04-15 00:26:05 +0800

    14每日一题-模拟

--- dataStructures/fenwick_tree.js
+++ dataStructures/fenwick_tree.js
@@ -1,9 +1,12 @@
+// lowbit函数 返回x的二进制位的最后一位1的位权
+// 例如：2 转化成二进制 10，故最后一位1的权重是2，同理 lowbit(3) =1,lowbit(4)=4
 const lowbit = (x) => x & -x;
 class FenwickTree {
     constructor(size) {
         this.n = size;
         this.c = Array(n + 1).fill(0);
     }
+    // 给原数组第i位 增加x
     add(i, x) {
         while (i <= this.n) {
             this.c[i] += x;
@@ -11,9 +14,11 @@ class FenwickTree {
         }
         return;
     }
+    // 查找原数组ind位上的数字
     at(ind) {
         return this.query(ind) - query(ind - 1);
     }
+    // 查询原数组前x项的和
     query(x) {
         let sum = 0;
         while (x) {
