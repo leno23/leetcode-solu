241952d95bc46f4c0209ae7e5cdef1db00e5c62f
wuyangfan <1102042793@qq.com>
2022-11-24 11:01:17 +0800

    1124-每日一题-单调栈

--- demo02.py
+++ demo02.py
@@ -1,7 +1,4 @@
-from heapq import *
-
-s = []
-heappush(s,2)
-heappush(s,5)
-heappush(s,3)
-print(s)
+t = int(input())
+for i in range(t):
+    a = list(map(int,input().split()))
+    print(sorted(a)[1])
