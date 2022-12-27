290db452c509a4ec2dafac0a857ac239f58c952f
wuyangfan <1102042793@qq.com>
2022-12-27 11:20:47 +0800

    update

--- 1.py
+++ 1.py
@@ -1,4 +1,8 @@
-print("""Hello!
-I'm PythonLover.
-This is my first solution.
-""")
+# s=input()
+
+# if s[0].isupper() and s[-1].isupper() and len(s)==8 and s[1:-1].isnumeric() and int(s[1:7])<=999999 and int(s[1:7]) >=100000:
+#     print('Yes')
+# else:
+#     print('No')
+from math import comb,e,factorial
+print(1_0000_000_7)
