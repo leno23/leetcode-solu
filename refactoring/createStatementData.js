10ab5992cd7d096f7c06b083b161ebbd565bcee3
wuyangfan <1102042793@qq.com>
2025-06-07 19:49:01 +0800

    feat: 阅读重构2 笔记-多态进一步优化

--- refactoring/createStatementData.js
+++ refactoring/createStatementData.js
@@ -1,16 +1,62 @@
 function createStatementData(invoice, plays) {
-  const statementData = {}
-  statementData.customer = invoice.customer
-  statementData.performances = invoice.performances.map(enrichPerformance)
-  statementData.totalAmount = totalAmount(statementData)
-  statementData.totalVolumnCredits = totalVolumnCredits(statementData)
-  return statementData
   function enrichPerformance(aPerformance) {
+    const calculator = createPerformCalculator(aPerformance, playFor(aPerformance))
     const result = Object.assign({}, aPerformance)
-    result.play = playFor(result)
-    result.amount = amountFor(result)
-    result.volumeCredits = volumeCreditsFor(result)
+    result.play = calculator.play
+    result.amount = calculator.amount
+    result.volumeCredits = calculator.volumeCredits
     return result
+  }
+
+  function createPerformCalculator(aPerformance, plays) {
+    let factory = {
+      tragedy: TragedyCalculator,
+      comedy: ComedyCalculator
+    }
+    let Calculator = factory[aPerformance.play.type]
+    if (!Calculator) {
+      throw new Error(`unknown type: ${aPerformance.play.type}`)
+    }
+    return new Calculator(aPerformance, plays)
+  }
+  //   演出计算器
+  class PerformanceCalculator {
+    constructor(aPerformance, plays) {
+      this.performance = aPerformance
+      this.plays = plays
+    }
+    get play() {
+      return playFor(this.performance)
+    }
+    get amount() {
+      throw new Error('子类必须实现 amount 方法')
+    }
+    get volumeCredits() {
+      return Math.max(this.performance.audience - 30, 0)
+    }
+  }
+  class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+      let result = 40000
+      if (this.performance.audience > 30) {
+        result += 1000 * (this.performance.audience - 30)
+      }
+      return result
+    }
+  }
+
+  class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+      let result = 30000
+      if (this.performance.audience > 20) {
+        result += 10000 + 500 * (this.performance.audience - 20)
+      }
+      result += 300 * this.performance.audience
+      return result
+    }
+    get volumeCredits() {
+      return super.volumeCredits + Math.floor(this.performance.audience / 5)
+    }
   }
 
   // 管道取代循环
@@ -22,48 +68,17 @@ function createStatementData(invoice, plays) {
     return data.performances.reduce((pre, cur) => cur.volumeCredits + pre, 0)
   }
 
-  // 提炼函数，将计算积分的逻辑抽离出来
-  function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0
-    // 观众超过30个的会奖励积分
-    volumeCredits += Math.max(aPerformance.audience - 30, 0)
-    // 喜剧每五个观众额外奖励一个积分
-    if ('comedy' === aPerformance.play.type) {
-      volumeCredits += Math.floor(aPerformance.audience / 5)
-    }
-    return volumeCredits
-  }
-
   // 以查询取代临时变量
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
   }
 
-  // 提炼函数，将计算费用部分的逻辑抽离出来
-  function amountFor(aPerformance) {
-    // 变量改名 thisAmount -> result
-    let result = 0
-
-    // calculate the amount for the performance
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`)
-    }
-    return result
-  }
+  const statementData = {}
+  statementData.customer = invoice.customer
+  statementData.performances = invoice.performances.map(enrichPerformance)
+  statementData.totalAmount = totalAmount(data.performances.reduce((pre, cur) => cur.amount + pre, 0))
+  statementData.totalVolumnCredits = totalVolumnCredits(statementData)
+  return statementData
 }
 
 export { createStatementData }
